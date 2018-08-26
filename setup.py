025be33d39aba7ac699c55ed4d6a2ca2b44a09c3
EvertonTomalok <evertontomalok123@gmail.com>
2018-08-26 16:52:32 -0300

    Setup

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
+
 setup(
-    name="emailtoolspython",
-    version="1.0.0",
-    packages=find_packages(),
-
-    install_requires=['requests>=2.19.1', 'beautifulsoup4>=4.4.0'],
-
-    # metadata to display on PyPI
+    name='emailtoolspython',
+    packages=['emailtoolspython'],
+    version='1.0.0',
     author="Everton Tomalok",
     author_email="evertontomalok123@gmail.com",
     description="A serie of methods to help you work with validation and extraction of e-mails",
+    url="https://github.com/EvertonTomalok/emailtoolspython",
+    download_url='https://github.com/EvertonTomalok/emailtoolspython/archive/master.zip',
+    keywords=['email', 'emails', 'tool', 'tools'],
+    classifiers=[],
+    install_requires=['requests>=2.19.1', 'beautifulsoup4>=4.4.0'],
     license="MIT",
-    keywords="email emails tool tools python python3",
-    url="https://github.com/EvertonTomalok/emailtoolspython"
 )
